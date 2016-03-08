ff77c1dde3998def80e8e1e735214bfe35e51a77
yangyong <398901650@qq.com>
2016-03-08 18:43:12 +0800

    增加了study项目

---
+++ dubbo-study/src/main/java/com/alibaba/dubbo/study/serialize/SerializeUtilStudy.java
@@ -0,0 +1,25 @@
+package com.alibaba.dubbo.study.serialize;
+
+import com.alibaba.dubbo.common.beanutil.JavaBeanDescriptor;
+import com.alibaba.dubbo.common.beanutil.JavaBeanSerializeUtil;
+
+/**
+ * Created by dell on 2016/3/8.
+ */
+public class SerializeUtilStudy {
+
+    public static void serializeJavaBean(){
+        StudyUser user = new StudyUser();
+        user.setAge(30);
+        user.setGender("男");
+        user.setName("测试");
+
+        JavaBeanDescriptor desc = JavaBeanSerializeUtil.serialize(user);
+        System.out.println(desc);
+        System.out.println(desc.getType());
+    }
+
+    public static void main(String[] args) {
+        SerializeUtilStudy.serializeJavaBean();
+    }
+}
---
+++ dubbo-study/src/main/java/com/alibaba/dubbo/study/serialize/StudyUser.java
@@ -0,0 +1,36 @@
+package com.alibaba.dubbo.study.serialize;
+
+/**
+ * Created by dell on 2016/3/8.
+ */
+public class StudyUser {
+    private String name;
+
+    private int age;
+
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
